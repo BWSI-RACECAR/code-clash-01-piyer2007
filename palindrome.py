3a7331d462ed92922288c48b991d4da1aabac527
piyer2007 <129233218+piyer2007@users.noreply.github.com>
2023-07-12 09:04:26 -0400

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -10,11 +10,11 @@ Author: Chris Lai
 
 Difficulty Level: 1/10
 
-Prompt: Paul has a brand new autonomous driving RACECAR that requires a password input 
-before unlocking and allowing the user to drive. Since Paul can never remember his exact 
-password, he set the following rule for a valid password: “If the password given to the 
+Prompt: Paul has a brand new autonomous driving RACECAR that requires a password input
+before unlocking and allowing the user to drive. Since Paul can never remember his exact
+password, he set the following rule for a valid password: “If the password given to the
 RACECAR is more than 6 letters long and is a palindrome, unlock the car”. Write a Python
-script that it returns “True” if the given password unlocks the car (is more than 6 
+script that it returns “True” if the given password unlocks the car (is more than 6
 characters long and is a palindrome) and returns “False” if the given password is invalid.
 
 Extra Challenge: Solve this problem in 1 line of code! (Excluding function definitions) 
@@ -30,8 +30,18 @@ class Solution:
     def isPalindrome(self, s):
             #type s: string
             #return type: boolean
-            
+
             #TODO: Write code below to return a boolean value with the solution to the prompt.
+            reverse = ""
+            for x in s:
+                reverse = x + reverse
+
+            if s == reverse:
+                return True
+
+            return False
+
+
             pass
 
 def main():
